39aa05551ea56f17a3741d8d62d3fdaf8f345911
Katarzyna Królikowska <katarzyna.anna.krolikowska@gmail.com>
2019-06-17 22:47:21 +0200

    add plugins for remote server

--- plugins/themehunk-customizer/shopline/woo/woo-inc.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once( plugin_dir_path(__FILE__) . '/woocommerce.php' );
-require_once( plugin_dir_path(__FILE__) . '/filter-woocommerce.php' );
-require_once( plugin_dir_path(__FILE__) . '/hooks-function.php' );
-require_once( plugin_dir_path(__FILE__) . '/hooks.php' );
-require_once( plugin_dir_path(__FILE__) . '/ajax-woocommerce.php' );
-?>
